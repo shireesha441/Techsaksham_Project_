3d11eea13ba5c25f384df0f2d68557b37d49e2dc
shireesha441 <105577760+shireesha441@users.noreply.github.com>
2022-11-24 11:08:02 +0530

    Add files via upload

---
+++ Code/category/DESERTS/exit1.js
@@ -0,0 +1,6 @@
+function back(){
+    window.location.href="../deserts.html";
+}
+function home(){
+    window.location.href="../../home.html";
+}
---
+++ Code/category/Drinks/exit1.js
@@ -0,0 +1,6 @@
+function back(){
+    window.location.href="../drinks.html";
+}
+function home(){
+    window.location.href="../../home.html";
+}
